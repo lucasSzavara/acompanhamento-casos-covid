e6843fc3c7d487e77e3d19af57040c84de2a080f
Lucas dos Santos Rodrigues Szavara <szavara.lucas.santos@gmail.com>
2021-07-25 17:56:49 -0300

    Usar arquivos no repositório

--- covid_ved/data.py
+++ covid_ved/data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('./datasets/caso_full.csv')
+df = pd.read_csv('https://media.githubusercontent.com/media/lucasSzavara/acompanhamento-casos-covid/main/covid_ved/datasets/caso_full.csv')
 print(df)
 df['uf'] = df['state']
 df['month'] = pd.to_datetime(df['last_available_date']).apply(lambda x: x.strftime('YYYY-MM'))
